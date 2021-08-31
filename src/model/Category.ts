87775f13478aea03afc4519b957e9aae256053d4
leosouza-dev <leonardo.89@uol.com.br>
2021-08-30 21:01:33 -0300

    iniciando a tipagem de categoria

---
+++ src/model/Category.ts
@@ -0,0 +1,19 @@
+import { v4 as uuid } from 'uuid';
+
+class Category {
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+
+    id?: string;
+
+    name: string;
+
+    description: string;
+
+    createdAt: Date;
+}
+
+export { Category };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,25 @@
 import { Router } from 'express';
-import { v4 as uuid } from 'uuid';
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
-  const category = {
+
+  const category = new Category();
+  //   category.name = name;
+  //   category.description = description;
+  //   category.createdAt = new Date();
+  Object.assign(category, { // daria no mesmo que ir atributo a atributo ou no ctor
     name,
     description,
-    id: uuid(),
-    createdAt: new Date(),
-  };
+    createdAd: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
