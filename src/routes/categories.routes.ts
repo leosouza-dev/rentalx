031b85869141932cd4a0612fffcf260d68c5888c
leosouza-dev <leonardo.89@uol.com.br>
2021-09-02 19:20:12 -0300

    Criando repositório de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,12 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
-
-  const category = new Category();
-  //   category.name = name;
-  //   category.description = description;
-  //   category.createdAt = new Date();
-  Object.assign(category, { // daria no mesmo que ir atributo a atributo ou no ctor
-    name,
-    description,
-    createdAd: new Date(),
-  });
-
-  categories.push(category);
+  const category = categoriesRepository.create({ name, description });
 
   return response.status(201).json({ category });
 });
