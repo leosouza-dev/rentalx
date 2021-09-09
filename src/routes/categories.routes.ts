e1c76b49852539984bdeb250205df456ed36531e
leosouza-dev <leonardo.89@uol.com.br>
2021-09-09 15:57:03 -0300

    Refatorando usando conceito de SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
-  const existsCategory = categoriesRepository.findByName(name);
-
-  if (existsCategory) return response.status(400).json({ error: 'Categoria existente' });
-
-  const category = categoriesRepository.create({ name, description });
-
-  return response.status(201).json(category);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
+  return response.status(201).send();
 });
 
 categoriesRoutes.get('/', (request, response) => {
